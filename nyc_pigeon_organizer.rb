bfb1c7504ae4878f12fe5ff1b0245c7ecb0b9e6b
Noah Eakin <noaheakin@gmail.com>
2020-07-20 20:49:46 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_data = data.each_with_object({}) do |(key, value), in_progress|
+    value.each do |inner_key, names|
+      names.each do |element|
+        if !in_progress[element]
+          in_progress[element] = {}
+        end
+        if !in_progress[element][key]
+          in_progress[element][key] = []
+        end
+        in_progress[element][key] << inner_key.to_s
+      end
+    end
+  end
+  new_data
 end
