540a45b0d297ba6eba61b95d88ecefa3eefb6e3a
Noah Eakin <noaheakin@gmail.com>
2020-08-10 22:39:09 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,16 +1,2 @@
 def nyc_pigeon_organizer (pigeon_data)
-  new_data = pigeon_data.each_with_object({}) do |(key, value), in_progress|
-    value.each do |inner_key, names|
-      names.each do |element|
-        if !in_progress[element]
-          in_progress[element] = {}
-        end
-        if !in_progress[element][key]
-          in_progress[element][key] = []
-        end
-        in_progress[element][key] << inner_key.to_s
-      end
-    end
-  end
-  new_data
-end
+  
